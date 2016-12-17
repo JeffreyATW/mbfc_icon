ed83e3009db74b4788efec935f711ed87044f868
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2016-12-16 20:28:24 -0800

    Better getHostName function, thx Mozilla

--- getTabSource.js
+++ getTabSource.js
@@ -1,11 +1,9 @@
 var getHostName = function (url) {
-  var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
-  if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
-    return match[2];
-  }
-  else {
-    return null;
-  }
+   try {
+       return new URL(url).hostname.match(/(www[0-9]?\.)?(.+)/i)[2];
+   } catch (e) {
+       return null; // Invalid URL
+   }
 }
 
 var getTabSource = function (url, cb) {
