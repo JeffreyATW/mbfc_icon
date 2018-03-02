2f7c4eba38f0806c1818c751ab25babf78f976b4
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2018-03-02 15:45:05 -0800

    Check for parent if domain is not in sources

--- getTabSource.js
+++ getTabSource.js
@@ -16,41 +16,56 @@ var getPath = function (url) {
 
 var getTabSource = function (url, cb) {
   browser.storage.local.get(['biases', 'sources'], function (items) {
-    var domain = getHostName(url);
+    var hostName = getHostName(url);
     var path = getPath(url);
     var domainSources;
     var source;
 
-    if (items.biases !== undefined && items.sources !== undefined) {
+    var getDomainSource = function (domain) {
       domainSources = items.sources[domain];
       // check if value is an array or object
-      if (domainSources && domainSources.length) {
-        // if array has more than one value
-        if (domainSources.length > 1) {
-          // iterate
-          for (var i = 0; i < domainSources.length; i += 1) {
-            // if source hasn't been set, settle for root path
-            if (domainSources[i].path === '/' && source === undefined) {
-              source = domainSources[i];
-            } else {
-              // check for source's path at beginning of current path
-              if (path.indexOf(domainSources[i].path) === 0) {
+      if (domainSources) {
+        if (domainSources.length) {
+          // if array has more than one value
+          if (domainSources.length > 1) {
+            // iterate
+            for (var i = 0; i < domainSources.length; i += 1) {
+              // if source hasn't been set, settle for root path
+              if (domainSources[i].path === '/' && source === undefined) {
                 source = domainSources[i];
-                // we found it, no need to iterate further
-                break;
+              } else {
+                // check for source's path at beginning of current path
+                if (path.indexOf(domainSources[i].path) === 0) {
+                  source = domainSources[i];
+                  // we found it, no need to iterate further
+                  break;
+                }
               }
             }
+            /* note: source could still be undefined if no domain source paths
+              were found in current path */
+          } else {
+            // just match with domain if only one value
+            source = domainSources[0];
           }
-          /* note: source could still be undefined if no domain source paths
-            were found in current path */
         } else {
-          // just match with domain if only one value
-          source = domainSources[0];
+          // not an array, must be slurping old JSON
+          source = domainSources;
         }
       } else {
-        // not an array, must be undefined or slurping old JSON
-        source = domainSources;
+        // see if current domain is actually subdomain and look for source of
+        // parent
+        const upDomainLevel = domain.match(/[^\.]+\.([^\.]+\..+)/);
+        if (upDomainLevel) {
+          return getDomainSource(upDomainLevel[1]);
+        }
       }
+      return source;
+    }
+
+    if (items.biases !== undefined && items.sources !== undefined) {
+      source = getDomainSource(hostName);
+
       if (source !== undefined) {
         var bias = items.biases[source.bias];
       }
